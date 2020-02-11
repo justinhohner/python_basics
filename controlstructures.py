24e3d7e2b5210d2805551be3b76fb89aeca0bdd6
justin hohner <justin.hohner@gmail.com>
2020-02-11 09:16:08 -0600

    control structures

---
+++ controlstructures.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+# https://www.tutorialsteacher.com/python/python-comparison-and-logical-operators
+
+# Comparisons
+a = 1
+b = 2
+
+print(a < b)
+print(a > b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# Logical operations
+print(a and b)
+print(a or b)
+print(not b)
+
+# Making decisions
+if a:
+    print('a is True')
+else:
+    print('a is False')
+
+# elif - else if can be stacked to check multiple conditions
